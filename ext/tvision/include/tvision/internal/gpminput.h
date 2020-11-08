98841150dbc59946ce05b663330943039665cfcc
Brian Luft <brian@electroly.com>
2020-11-08 01:27:38 +0000

    Remove gpm stuff from tvision
    Former-commit-id: ac0bde8771a957bac1e290f59a09c2e87042c55f

--- ext/tvision/include/tvision/internal/gpminput.h
+++ ext/tvision/include/tvision/internal/gpminput.h
@@ -3,35 +3,6 @@
 
 #include <internal/platform.h>
 
-#ifdef HAVE_GPM
-
-#define Uses_TPoint
-#define Uses_TEvent
-#include <tvision/tv.h>
-
-#include <internal/cursor.h>
-#include <gpm.h>
-
-class GpmInput : public FdInputStrategy {
-
-    ScreenCursor cursor;
-    uchar buttonState;
-
-    static void fitEvent(Gpm_Event&);
-
-public:
-
-    GpmInput();
-    ~GpmInput();
-    bool getEvent(TEvent &ev);
-    int getButtonCount();
-
-};
-
-#else
-
 class GpmInput : public FdInputStrategy {};
-
-#endif // HAVE_GPM
 
 #endif
