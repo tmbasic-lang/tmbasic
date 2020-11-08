d581d71f70d084c36b1e2a68bd7f52092fa24cdd
Brian Luft <brian@electroly.com>
2020-11-08 06:37:38 +0000

    Stub out runner
    Former-commit-id: c20bc9bf07524df3f739a43df2830d2f89d464e7

--- src/runner/main.cpp
+++ src/runner/main.cpp
@@ -1,3 +1,27 @@
+#include "common.h"
+
+static uint8_t* findAppendedData() {
+    auto pid = getpid();
+    auto s = std::ostringstream();
+    s << "/proc/" << pid << "/exe";
+    auto exeFilePath = s.str();
+    auto file = std::ifstream(exeFilePath);
+
+    // the last three bytes are "TM\n"
+    file.seekg(-3, std::ios::end);
+    char sentinel[3] = { 0 };
+    file.read(sentinel, 3);
+    if (sentinel[0] != 'T' || sentinel[1] != 'M' || sentinel[2] != '\n') {
+        std::cerr << "Cannot find a compiled program in this executable file." << std::endl;
+        exit(-1);
+        return nullptr;
+    }
+
+    //TODO
+    return nullptr;
+}
+
 int main(int argc, char** argv) {
+    findAppendedData();
     return 0;
 }
