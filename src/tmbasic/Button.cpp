15faa1d62a8d37455780b586416bfd89648725d5
Brian Luft <brian@electroly.com>
2020-12-12 08:47:42 +0000

    Fix flags parameter in Button constructor

--- src/tmbasic/Button.cpp
+++ src/tmbasic/Button.cpp
@@ -6,6 +6,6 @@ static const auto kButtonWidth = 13;
 static const auto kButtonHeight = 2;
 
 Button::Button(TStringView aTitle, ushort aCommand, ushort aFlags)
-    : TButton(TRect(0, 0, kButtonWidth, kButtonHeight), aTitle, aCommand, flags) {}
+    : TButton(TRect(0, 0, kButtonWidth, kButtonHeight), aTitle, aCommand, aFlags) {}
 
 }  // namespace tmbasic
