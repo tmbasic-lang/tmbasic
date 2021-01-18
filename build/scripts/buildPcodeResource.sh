20fa0c36051c68fbd5d71a98023ba3c0f34ed6fa
Brian Luft <brian@electroly.com>
2021-01-18 17:25:13 +0000

    Move dummy pcode files

--- build/scripts/buildPcodeResource.sh
+++ build/scripts/buildPcodeResource.sh
@@ -4,9 +4,9 @@
 # file into an object using xxd.
 set -euo pipefail
 export NUM_BYTES=$(echo -n $OBJ_FILE | sed 's/[^0-9]//g')
-export DATA_FILE=obj/pcode$NUM_BYTES
+export DATA_FILE=obj/resources/pcode/pcode$NUM_BYTES
 export SYMBOL=obj_pcode$NUM_BYTES
 export SRC_FILE=obj/resources/pcode/$NUM_BYTES.cpp
 head -c $NUM_BYTES /dev/zero | tr '\0' 'T' > $DATA_FILE
-xxd -i $DATA_FILE | sed s/obj_pcode$NUM_BYTES/kResourcePcode/g > $SRC_FILE
+xxd -i $DATA_FILE | sed s/obj_resources_pcode_pcode$NUM_BYTES/kResourcePcode/g > $SRC_FILE
 $CXX -o $OBJ_FILE -c $SRC_FILE
