25791a2f10b26baa5799810ad635eec03740f95e
Brian Luft <brian@electroly.com>
2021-02-06 03:01:27 +0000

    Add ICU test for word boundary analysis

--- src/test/IcuTest.cpp
+++ src/test/IcuTest.cpp
@@ -78,3 +78,71 @@ TEST(IcuTest, StringComparison) {
     auto collator = std::unique_ptr<Collator>(Collator::createInstance(Locale::getUS(), status));
     ASSERT_EQ(UCOL_EQUAL, collator->compare(str1, str2));
 }
+
+TEST(IcuTest, Words1) {
+    auto status = U_ZERO_ERROR;
+    const auto* str = "a dog.  the\tcat-dog?";
+    auto ustr = UnicodeString::fromUTF8(str);
+    auto iter = std::unique_ptr<BreakIterator>(BreakIterator::createWordInstance(Locale::getUS(), status));
+    iter->setText(ustr);
+    auto index = iter->first();
+    std::vector<int32_t> indices;
+    while (index != BreakIterator::DONE) {
+        indices.push_back(index);
+        index = iter->next();
+    }
+
+    // 0
+    // 1 "a"
+    // 2 " "
+    // 5 "dog"
+    // 6 "."
+    // 8 "  "
+    // 11 "the"
+    // 12 "\t"
+    // 15 "cat"
+    // 16 "-"
+    // 19 "dog"
+    // 20 "?"
+
+    ASSERT_EQ(12, indices.size());
+    ASSERT_EQ(0, indices.at(0));
+    ASSERT_EQ(1, indices.at(1));
+    ASSERT_EQ(2, indices.at(2));
+    ASSERT_EQ(5, indices.at(3));
+    ASSERT_EQ(6, indices.at(4));
+    ASSERT_EQ(8, indices.at(5));
+    ASSERT_EQ(11, indices.at(6));
+    ASSERT_EQ(12, indices.at(7));
+    ASSERT_EQ(15, indices.at(8));
+    ASSERT_EQ(16, indices.at(9));
+    ASSERT_EQ(19, indices.at(10));
+    ASSERT_EQ(20, indices.at(11));
+}
+
+TEST(IcuTest, Words2) {
+    auto status = U_ZERO_ERROR;
+    const auto* str = "これはテストです";
+    auto ustr = UnicodeString::fromUTF8(str);
+    auto iter = std::unique_ptr<BreakIterator>(BreakIterator::createWordInstance(Locale::getJapan(), status));
+    iter->setText(ustr);
+    auto index = iter->first();
+    std::vector<int32_t> indices;
+    while (index != BreakIterator::DONE) {
+        indices.push_back(index);
+        index = iter->next();
+    }
+
+    // 0
+    // 2 "これ"
+    // 3 "は"
+    // 6 "テスト"
+    // 8 "です"
+
+    ASSERT_EQ(5, indices.size());
+    ASSERT_EQ(0, indices.at(0));
+    ASSERT_EQ(2, indices.at(1));
+    ASSERT_EQ(3, indices.at(2));
+    ASSERT_EQ(6, indices.at(3));
+    ASSERT_EQ(8, indices.at(4));
+}
