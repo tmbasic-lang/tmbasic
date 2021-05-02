b550da6c15f4a3616f83e912e5b1ad05d0ff0f22
Brian Luft <brian@electroly.com>
2021-05-02 02:55:55 +0000

    Fix logo in about dialog

--- src/tmbasic/AboutDialog.cpp
+++ src/tmbasic/AboutDialog.cpp
@@ -13,9 +13,9 @@ using util::PictureView;
 namespace tmbasic {
 
 static const std::string kLogoPicture =
-    "picture Logo Z 2e 7 N e29684 800080 c0c0c0 A A A A A A A B 20 E ff A A A A E ffffff A A A A A A A A A A A A A "
-    "A A A A A A A A A A A A A A A A A A F e29680 800080 A A B e29688 A B e29680 A A B 20 E ff A A A A B e29688 A A A "
-    "A B e29684 F 20 ffffff F e29684 ff B e29688 A A A B e29684 F 20 ffffff F e29684 ff B e29688 A A A B e29684 F 20 "
+    "picture Logo Z 2e 7 N e29684 800080 c0c0c0 A A A A A A A B 20 E ff A A A A E ffffff A A A A A A A A A A A A A A A "
+    "A A A A A A A A A A A A A A A A F e29680 800080 A A B e29688 A B e29680 A A B 20 E ff A A A A B e29688 A A A A B "
+    "e29684 F 20 ffffff F e29684 ff B e29688 A A A B e29684 F 20 ffffff F e29684 ff B e29688 A A A B e29684 F 20 "
     "ffffff F e29688 ff A A A F 20 ffffff F e29684 ff B e29688 A A A B e29684 B 20 A A F e29688 800080 A F 20 ff F "
     "e29688 800080 A B 20 E ff E 800080 B e29688 A B 20 F e29688 ff A F 20 ffffff A F e29688 ff A F 20 ffffff F e29688 "
     "ff A F 20 ffffff A F e29688 ff A F 20 ffffff F e29688 ff A F 20 ffffff A F e29680 ff A F 20 ffffff A F e29688 ff "
@@ -24,11 +24,11 @@ static const std::string kLogoPicture =
     "A A B e29684 F 20 ffffff A F e29688 ff A F 20 ffffff A F e29688 ff A F 20 ffffff A A A E ff A A F e29688 800080 A "
     "B 20 B e29688 A B e29680 B e29688 B e29680 B e29688 A F 20 ff B e29688 A F 20 ffffff A F e29688 ff A F 20 ffffff "
     "F e29688 ff A F 20 ffffff A F e29688 ff A F 20 ffffff F e29684 ff A F 20 ffffff A F e29688 ff A F 20 ffffff A F "
-    "e29688 ff A F 20 ffffff A F e29688 ff A F 20 ffffff A F e29684 ff A B 20 A A A A A F e29688 800080 A F 20 ff A A "
-    "F e29688 800080 A F 20 ff B e29688 A A A A B e29680 F 20 ffffff F e29688 ff A F 20 ffffff A F e29688 ff A F 20 "
-    "ffffff F e29680 ff B e29688 A A A B e29680 F 20 ffffff F e29688 ff A A A F 20 ffffff F e29680 ff B e29688 A A A B "
-    "e29680 F 20 ffffff A A A E ff A F e29688 800080 A F 20 ff A A F e29688 800080 A F 20 ff E ffffff A A A A A A A A "
-    "A E ff A A A A A A A A A A A A A A A A A A A A A end picture";
+    "e29688 ff A F 20 ffffff A F e29688 ff A F 20 ffffff A F e29684 ff A B 20 A A F e29680 800080 A F 20 ff F e29688 "
+    "800080 A F 20 ff A A F e29688 800080 A F 20 ff B e29688 A A A A B e29680 F 20 ffffff F e29688 ff A F 20 ffffff A "
+    "F e29688 ff A F 20 ffffff F e29680 ff B e29688 A A A B e29680 F 20 ffffff F e29688 ff A A A F 20 ffffff F e29680 "
+    "ff B e29688 A A A B e29680 F 20 ffffff A A A E ff A F e29688 800080 A F 20 ff A A F e29688 800080 A F 20 ff E "
+    "ffffff A A A A A A A A A E ff A A A A A A A A A A A A A A A A A A A A A end picture";
 
 AboutDialog::AboutDialog() : TDialog(TRect(0, 0, 0, 0), "About TMBASIC"), TWindowInit(&TDialog::initFrame) {
     GridLayout(
