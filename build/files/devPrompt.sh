993bdf52552b6825c58e7e7a41de848f7441ed31
Brian Luft <brian@electroly.com>
2021-08-01 05:44:01 +0000

    Doc for ErrorCode(), ErrorMessage()

--- build/files/devPrompt.sh
+++ build/files/devPrompt.sh
@@ -3,7 +3,7 @@ while true
 do
     echo -e "\e[1;30mcompile:\e[0m \e[1;33mm\e[0make \e[1;33mg\e[0mhpages \e[1;33mc\e[0mlean"
     echo -e "\e[1;30mrun:\e[0m     \e[1;33mr\e[0mun \e[1;33m1\e[0m6color \e[1;33m2\e[0m56color \e[1;33mt\e[0mest"
-    echo -e "\e[1;30medit:\e[0m    \e[1;33mf\e[0mormat \e[1;33mu\e[0mpdateCompilerTest"
+    echo -e "\e[1;30medit:\e[0m    \e[1;33mf\e[0mormat \e[1;33mu\e[0mpdateCompilerTest update\e[1;33mE\e[0mrrors"
     echo -n "───────> "
     read -n1 x
     echo
@@ -17,6 +17,9 @@ do
     elif [ "$x" == "u" ]
     then
         build/scripts/updateCompilerTest.sh
+    elif [ "$x" == "e" ]
+    then
+        build/scripts/updateErrors.sh
     elif [ "$x" == "1" ]
     then
         TERM=xterm COLORTERM= make run
--- build/scripts/updateErrors.sh
+++ build/scripts/updateErrors.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-# updates src/compiler/BuiltInConstantList.cpp based on the errors in src/vm/Error.h
+# updates BuiltInConstantList.cpp and ref_constants.txt based on the errors in src/vm/Error.h
 
+# src/compiler/BuiltInConstantList.cpp
 grep -v ERR_ src/compiler/BuiltInConstantList.cpp > /tmp/list.cpp
 export LINE_NUMBER=$(grep -n "// start_errors" /tmp/list.cpp | awk -F: '{ print $1 }')
 head -n $LINE_NUMBER /tmp/list.cpp > /tmp/list2.cpp
-cat src/vm/Error.h | grep ERR_ | awk '{ print "    addError(\"" $5 "\", vm::ErrorCode::" $1 ");" }' >> /tmp/list2.cpp
+grep ERR_ src/vm/Error.h | awk '{ print "    addError(\"" $5 "\", vm::ErrorCode::" $1 ");" }' >> /tmp/list2.cpp
 tail -n +$(($LINE_NUMBER+1)) /tmp/list.cpp >> /tmp/list2.cpp
 mv -f /tmp/list2.cpp src/compiler/BuiltInConstantList.cpp
 rm -f /tmp/list.cpp
+
+# doc/help/topics/ref_constants.txt
+grep -v "li@" doc/help/topics/ref_constants.txt > /tmp/help.txt
+export LINE_NUMBER=$(grep -n "ul@" /tmp/help.txt | awk -F: '{ print $1 }')
+head -n $LINE_NUMBER /tmp/help.txt > /tmp/help2.txt
+grep ERR_ src/vm/Error.h | awk '{ print "li@`" $5 "`@" }' | sort >> /tmp/help2.txt
+tail -n +$(($LINE_NUMBER+1)) /tmp/help.txt >> /tmp/help2.txt
+mv -f /tmp/help2.txt doc/help/topics/ref_constants.txt
+rm -f /tmp/help.txt
