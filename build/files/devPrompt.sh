11ab11ec109152151f45c8b84c4f74a33704b9b4
Brian Luft <brian@electroly.com>
2021-08-03 01:58:45 +0000

    Add testFilter command to devPrompt.sh

--- build/files/devPrompt.sh
+++ build/files/devPrompt.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 while true
 do
+    echo
     echo -e "\e[1;30mcompile:\e[0m \e[1;33mm\e[0make \e[1;33mg\e[0mhpages \e[1;33mc\e[0mlean"
-    echo -e "\e[1;30mrun:\e[0m     \e[1;33mr\e[0mun \e[1;33m1\e[0m6color \e[1;33m2\e[0m56color \e[1;33mt\e[0mest"
-    echo -e "\e[1;30medit:\e[0m    \e[1;33mf\e[0mormat \e[1;33mu\e[0mpdateCompilerTest update\e[1;33mE\e[0mrrors"
+    echo -e "\e[1;30mrun:\e[0m     \e[1;33mr\e[0mun \e[1;33m1\e[0m6color \e[1;33m2\e[0m56color \e[1;33mt\e[0mest test\e[1;33mF\e[0milter"
+    echo -e "\e[1;30medit:\e[0m    \e[1;33mf\e[0mormat \e[1;33mu\e[0mpdateCompilerTest updat\e[1;33me\e[0mErrors"
     echo -n "───────> "
     read -n1 x
     echo
@@ -31,7 +32,16 @@ do
         make format
     elif [ "$x" == "t" ]
     then
+        export GTEST_FILTER="*"
+        if [ -f "test_filter" ]; then
+            export GTEST_FILTER=$(cat test_filter)
+        fi
         make test
+    elif [ "$x" == "F" ]
+    then
+        echo -n "GTEST_FILTER="
+        read GTEST_FILTER
+        echo "$GTEST_FILTER" > test_filter
     elif [ "$x" == "g" ]
     then
         make ghpages
