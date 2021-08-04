b31bdf9c81116871fd9fe611573ed77a36573954
Brian Luft <brian@electroly.com>
2021-08-04 07:05:36 +0000

    Reorganize docs

--- build/scripts/updateErrors.sh
+++ build/scripts/updateErrors.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# updates BuiltInConstantList.cpp and ref_constants.txt based on the errors in src/vm/Error.h
+# updates BuiltInConstantList.cpp and builtin_constants.txt based on the errors in src/vm/Error.h
 
 # src/compiler/BuiltInConstantList.cpp
 grep -v ERR_ src/compiler/BuiltInConstantList.cpp > /tmp/list.cpp
@@ -10,11 +10,11 @@ tail -n +$(($LINE_NUMBER+1)) /tmp/list.cpp >> /tmp/list2.cpp
 mv -f /tmp/list2.cpp src/compiler/BuiltInConstantList.cpp
 rm -f /tmp/list.cpp
 
-# doc/help/topics/ref_constants.txt
-grep -v "li@" doc/help/topics/ref_constants.txt > /tmp/help.txt
+# doc/help/topics/builtin_constants.txt
+grep -v "li@" doc/help/topics/builtin_constants.txt > /tmp/help.txt
 export LINE_NUMBER=$(grep -n "ul@" /tmp/help.txt | awk -F: '{ print $1 }')
 head -n $LINE_NUMBER /tmp/help.txt > /tmp/help2.txt
 grep ERR_ src/vm/Error.h | awk '{ print "li@`" $5 "`@" }' | sort >> /tmp/help2.txt
 tail -n +$(($LINE_NUMBER+1)) /tmp/help.txt >> /tmp/help2.txt
-mv -f /tmp/help2.txt doc/help/topics/ref_constants.txt
+mv -f /tmp/help2.txt doc/help/topics/builtin_constants.txt
 rm -f /tmp/help.txt
