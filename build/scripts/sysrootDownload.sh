789710cfb15360b387b2cfc25e45c765cd331cec
Brian Luft <brian@electroly.com>
2023-11-09 02:41:51 +0000

    CI: try building build environments

--- build/scripts/sysrootDownload.sh
+++ build/scripts/sysrootDownload.sh
@@ -3,22 +3,30 @@
 # run from the build/ directory
 set -eu
 
+export SYSROOT_VERSION="unknown_arch"
+
 if [ "$ARCH" = "arm32v7" ]; then
-    export SYSROOT_VERSION="arm32v7-20230108041738"
+    export SYSROOT_VERSION=$(awk -F= '{ if ($1 == "linux-system-root-arm32") print $2 }' tags.ini)
 fi
 if [ "$ARCH" = "arm64v8" ]; then
-    export SYSROOT_VERSION="arm64v8-20230108042227"
+    export SYSROOT_VERSION=$(awk -F= '{ if ($1 == "linux-system-root-arm64") print $2 }' tags.ini)
 fi
 if [ "$ARCH" = "x86_64" ]; then
-    export SYSROOT_VERSION="amd64-20230108042628"
+    export SYSROOT_VERSION=$(awk -F= '{ if ($1 == "linux-system-root-x64") print $2 }' tags.ini)
 fi
 if [ "$ARCH" = "i686" ]; then
-    export SYSROOT_VERSION="i386-20230108042000"
+    export SYSROOT_VERSION=$(awk -F= '{ if ($1 == "linux-system-root-x86") print $2 }' tags.ini)
 fi
+
+export IMAGE_NAME="ghcr.io/electroly/tmbasic-linux-sysroot:$SYSROOT_VERSION"
+export FILENAME="downloads/sysroot-$SYSROOT_VERSION.tar.gz"
 
 mkdir -p downloads
 if [ ! -f "downloads/sysroot-$SYSROOT_VERSION.tar.gz" ]; then
-    echo "Downloading s3://tmbasic/linux-sysroots/sysroot-$SYSROOT_VERSION.tar.gz..."
-    aws s3 cp s3://tmbasic/linux-sysroots/sysroot-$SYSROOT_VERSION.tar.gz "downloads/sysroot-$SYSROOT_VERSION.tar.gz" --request-payer
+    echo "Pulling $IMAGE_NAME..."
+    docker pull $IMAGE_NAME
+
+    echo "Saving to tar.gz..."
+    docker save $IMAGE_NAME | gzip > "$FILENAME"
 fi
-cp -f "downloads/sysroot-$SYSROOT_VERSION.tar.gz" "files/sysroot-$ARCH.tar.gz"
+cp -f "$FILENAME" "files/sysroot-$ARCH.tar.gz"
--- build/win-x64.sh
+++ build/win-x64.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# Parameters: $IMAGE_NAME (optional), $PUSH_ONLY (optional)
 set -euo pipefail
 
-export IMAGE_NAME="tmbasic-win-x64"
+export IMAGE_NAME=${IMAGE_NAME:="tmbasic-win-x64"}
 export HOST_UID=$(id -u "$USER")
 export HOST_GID=$(id -g "$USER")
 export ARCH="x86_64"
@@ -15,9 +16,18 @@ fi
 
 scripts/depsDownload.sh
 
-if [ "$(docker image ls $IMAGE_NAME | wc -l)" == "1" ]; then
-    cat files/Dockerfile.build-win | sed "s/\$IMAGE_NAME/$IMAGE_NAME/g; s/\$HOST_UID/$HOST_UID/g; s/\$HOST_GID/$HOST_GID/g; s/\$ARCH/$ARCH/g; s/\$LIB3264/$LIB3264/g; s/\$WINE_INSTALL/$WINE_INSTALL/g" | docker buildx build -t $IMAGE_NAME files -f-
+# If $PUSH_ONLY is non-empty, then set the --push argument.
+export BUILDX_FLAGS=""
+if [ -n "${PUSH_ONLY+x}" ]; then
+    export BUILDX_FLAGS="--push"
 fi
 
-cd ..
-docker run --rm ${TTY_FLAG:=--tty --interactive} --volume "$PWD:/code" --workdir /code --name $IMAGE_NAME ${DOCKER_FLAGS:= } $IMAGE_NAME "$@"
+if [ "$(docker image ls $IMAGE_NAME | wc -l)" == "1" ]; then
+    cat files/Dockerfile.build-win | sed "s/\$IMAGE_NAME/$IMAGE_NAME/g; s/\$HOST_UID/$HOST_UID/g; s/\$HOST_GID/$HOST_GID/g; s/\$ARCH/$ARCH/g; s/\$LIB3264/$LIB3264/g; s/\$WINE_INSTALL/$WINE_INSTALL/g" | docker buildx build $BUILDX_FLAGS -t $IMAGE_NAME files -f-
+fi
+
+# If $PUSH_ONLY is empty or unset, then run.
+if [ -z "${PUSH_ONLY+x}" ]; then
+    cd ..
+    docker run --rm ${TTY_FLAG:=--tty --interactive} --volume "$PWD:/code" --workdir /code --name $IMAGE_NAME ${DOCKER_FLAGS:= } $IMAGE_NAME "$@"
+fi
