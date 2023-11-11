515f8320fde2128fde11565c2d09856fe7b99bc8
Brian Luft <brian@electroly.com>
2023-11-11 18:26:02 +0000

    CI: check Mac host type before doing anything else

--- build/mac-x64.sh
+++ build/mac-x64.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -euxo pipefail
+
+# Verify that the host macOS system is ARM64, NOT Intel.
+echo "Host type: $(uname -m)"
+if [ "$(uname -m)" != "arm64" ]; then
+    echo "This script must be run on an Apple Silicon Mac."
+    exit 1
+fi
+
 scripts/depsDownload.sh
 ARCH="x86_64" scripts/macSetup.sh "$@"
--- build/scripts/macSetup.sh
+++ build/scripts/macSetup.sh
@@ -4,13 +4,6 @@
 # run from the build directory.
 set -euo pipefail
 
-# Verify that the host macOS system is ARM64, NOT Intel.
-echo "Host type: $(uname -m)"
-if [ "$(uname -m)" != "arm64" ]; then
-    echo "This script must be run on an Apple Silicon Mac."
-    exit 1
-fi
-
 scripts/depsDownload.sh
 
 rm -rf deps
