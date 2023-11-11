fda532e745b995740e6581590c5e91a35e6f3614
Brian Luft <brian@electroly.com>
2023-11-11 20:45:04 +0000

    CI: build tmbasic

--- build/linux-arm32.sh
+++ build/linux-arm32.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Optional parameters: $IMAGE_NAME, $PUSH_ONLY, $HOST_UID, $HOST_GID
+# Optional parameters: $IMAGE_NAME, $NO_BUILD, $PUSH_ONLY, $HOST_UID, $HOST_GID
 set -euxo pipefail
 
 export IMAGE_NAME=${IMAGE_NAME:="tmbasic-linux-arm32"}
@@ -9,17 +9,20 @@ export DOCKER_ARCH="arm32v7"
 export ARCH="arm32v7"
 export TRIPLE="armv7-alpine-linux-musleabihf"
 
-scripts/sysrootDownload.sh
-scripts/depsDownload.sh
+# Build unless $NO_BUILD is non-empty.
+if [ -z "${NO_BUILD+x}" ]; then
+    scripts/sysrootDownload.sh
+    scripts/depsDownload.sh
 
-# If $PUSH_ONLY is non-empty, then set the --push argument.
-export BUILDX_FLAGS=""
-if [ -n "${PUSH_ONLY+x}" ]; then
-    export BUILDX_FLAGS="--push"
-fi
+    # If $PUSH_ONLY is non-empty, then set the --push argument.
+    export BUILDX_FLAGS=""
+    if [ -n "${PUSH_ONLY+x}" ]; then
+        export BUILDX_FLAGS="--push"
+    fi
 
-if [ "$(docker image ls $IMAGE_NAME | wc -l)" == "1" ]; then
-    cat files/Dockerfile.build-linux | sed "s=\$IMAGE_NAME=$IMAGE_NAME=g; s/\$HOST_UID/$HOST_UID/g; s/\$HOST_GID/$HOST_GID/g; s/\$DOCKER_ARCH/$DOCKER_ARCH/g; s/\$ARCH/$ARCH/g; s/\$USER/$USER/g; s/\$TRIPLE/$TRIPLE/g" | docker buildx build $BUILDX_FLAGS -t $IMAGE_NAME files -f-
+    if [ "$(docker image ls $IMAGE_NAME | wc -l)" == "1" ]; then
+        cat files/Dockerfile.build-linux | sed "s=\$IMAGE_NAME=$IMAGE_NAME=g; s/\$HOST_UID/$HOST_UID/g; s/\$HOST_GID/$HOST_GID/g; s/\$DOCKER_ARCH/$DOCKER_ARCH/g; s/\$ARCH/$ARCH/g; s/\$USER/$USER/g; s/\$TRIPLE/$TRIPLE/g" | docker buildx build $BUILDX_FLAGS -t $IMAGE_NAME files -f-
+    fi
 fi
 
 # If $PUSH_ONLY is empty or unset, then run.
--- build/win-x86.sh
+++ build/win-x86.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Optional parameters: $IMAGE_NAME, $PUSH_ONLY, $HOST_UID, $HOST_GID
+# Optional parameters: $IMAGE_NAME, $NO_BUILD, $PUSH_ONLY, $HOST_UID, $HOST_GID
 set -euxo pipefail
 
 export IMAGE_NAME=${IMAGE_NAME:="tmbasic-win-x86"}
@@ -14,16 +14,19 @@ else
 export WINE_INSTALL="RUN echo Not installing wine on this platform."
 fi
 
-scripts/depsDownload.sh
+# Build unless $NO_BUILD is non-empty.
+if [ -z "${NO_BUILD+x}" ]; then
+    scripts/depsDownload.sh
 
-# If $PUSH_ONLY is non-empty, then set the --push argument.
-export BUILDX_FLAGS=""
-if [ -n "${PUSH_ONLY+x}" ]; then
-    export BUILDX_FLAGS="--push"
-fi
+    # If $PUSH_ONLY is non-empty, then set the --push argument.
+    export BUILDX_FLAGS=""
+    if [ -n "${PUSH_ONLY+x}" ]; then
+        export BUILDX_FLAGS="--push"
+    fi
 
-if [ "$(docker image ls $IMAGE_NAME | wc -l)" == "1" ]; then
-    cat files/Dockerfile.build-win | sed "s=\$IMAGE_NAME=$IMAGE_NAME=g; s/\$HOST_UID/$HOST_UID/g; s/\$HOST_GID/$HOST_GID/g; s/\$ARCH/$ARCH/g; s/\$LIB3264/$LIB3264/g; s/\$WINE_INSTALL/$WINE_INSTALL/g" | docker buildx build $BUILDX_FLAGS -t $IMAGE_NAME files -f-
+    if [ "$(docker image ls $IMAGE_NAME | wc -l)" == "1" ]; then
+        cat files/Dockerfile.build-win | sed "s=\$IMAGE_NAME=$IMAGE_NAME=g; s/\$HOST_UID/$HOST_UID/g; s/\$HOST_GID/$HOST_GID/g; s/\$ARCH/$ARCH/g; s/\$LIB3264/$LIB3264/g; s/\$WINE_INSTALL/$WINE_INSTALL/g" | docker buildx build $BUILDX_FLAGS -t $IMAGE_NAME files -f-
+    fi
 fi
 
 # If $PUSH_ONLY is empty or unset, then run.
