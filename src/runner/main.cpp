bf45e221cbf270565773d4c76f8454d63271d6c9
Brian Luft <brian@electroly.com>
2024-01-15 08:24:22 +0000

    Print uncaught error messages

--- src/runner/main.cpp
+++ src/runner/main.cpp
@@ -8,22 +8,39 @@
 extern const uint8_t kResourcePcode[];  // NOLINT(cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays)
 extern const uint kResourcePcode_len;
 
-int main(int /*argc*/, const char* /*argv*/[]) {
-    shared::setUtf8Locale();
-    vm::initializeTzdb();
-
+static void deserializePcode(vm::Program* program) {
     std::vector<uint8_t> pcode{};
     pcode.reserve(kResourcePcode_len);
     // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
     pcode.insert(pcode.end(), kResourcePcode, kResourcePcode + kResourcePcode_len);
+    program->deserialize(pcode);
+}
 
-    vm::Program program{};
-    program.deserialize(pcode);
+int main(int /*argc*/, const char* /*argv*/[]) {
+    try {
+        shared::setUtf8Locale();
+        vm::initializeTzdb();
 
-    vm::Interpreter interpreter{ &program, &std::cin, &std::cout };
-    interpreter.init(program.startupProcedureIndex);
-    while (interpreter.run(10000)) {
+        vm::Program program{};
+        deserializePcode(&program);
+
+        vm::Interpreter interpreter{ &program, &std::cin, &std::cout };
+        interpreter.init(program.startupProcedureIndex);
+        while (interpreter.run(10000)) {
+        }
+
+        auto error = interpreter.getError();
+        if (error.has_value()) {
+            std::cerr << "Error " << error->code.getString() << ": " << error->message << std::endl;
+            return 1;
+        }
+
+        return 0;
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    } catch (...) {
+        std::cerr << "Unknown fatal error." << std::endl;
+        return 1;
     }
-
-    return 0;
 }
