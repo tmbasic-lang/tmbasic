f867cd138098a4df32dab913997a2d1c1df39b8b
Brian Luft <brian@electroly.com>
2024-02-09 06:03:55 +0000

    Add path tests

--- src/test/PathTest.cpp
+++ src/test/PathTest.cpp
@@ -6,6 +6,10 @@
 
 using shared::Error;
 using shared::getDirectoryName;
+using shared::getExtension;
+using shared::getFileName;
+using shared::getFileNameWithoutExtension;
+using shared::pathCombine;
 
 TEST(PathTest, GetDirectoryName_EmptyPath) {
     std::string filePath = "";
@@ -26,6 +30,31 @@ TEST(PathTest, GetDirectoryName_MacOSBehavior) {
     std::string expectedDir = "/path/to";
     EXPECT_EQ(getDirectoryName(filePath), expectedDir);
 }
+
+TEST(PathTest, GetFileName_MacOSBehavior) {
+    std::string filePath = "/path/to/file.txt";
+    std::string expectedFile = "file.txt";
+    EXPECT_EQ(getFileName(filePath), expectedFile);
+}
+
+TEST(PathTest, GetFileNameWithoutExtension_MacOSBehavior) {
+    std::string filePath = "/path/to/file.txt";
+    std::string expectedFile = "file";
+    EXPECT_EQ(getFileNameWithoutExtension(filePath), expectedFile);
+}
+
+TEST(PathTest, GetExtension_MacOSBehavior) {
+    std::string filePath = "/path/to/file.txt";
+    std::string expectedFile = ".txt";
+    EXPECT_EQ(getExtension(filePath), expectedFile);
+}
+
+TEST(PathTest, PathCombine_MacOSBehavior) {
+    std::string a = "/path/to";
+    std::string b = "file.txt";
+    std::string expected = "/path/to/file.txt";
+    EXPECT_EQ(pathCombine(a, b), expected);
+}
 #endif
 
 #ifdef _WIN32
@@ -35,6 +64,31 @@ TEST(PathTest, GetDirectoryName_WindowsBehavior) {
     std::string expectedDir = "C:\\path\\to";
     EXPECT_EQ(getDirectoryName(filePath), expectedDir);
 }
+
+TEST(PathTest, GetFileName_WindowsBehavior) {
+    std::string filePath = "C:\\path\\to\\file.txt";
+    std::string expectedFile = "file.txt";
+    EXPECT_EQ(getFileName(filePath), expectedFile);
+}
+
+TEST(PathTest, GetFileNameWithoutExtension_WindowsBehavior) {
+    std::string filePath = "C:\\path\\to\\file.txt";
+    std::string expectedFile = "file";
+    EXPECT_EQ(getFileNameWithoutExtension(filePath), expectedFile);
+}
+
+TEST(PathTest, GetExtension_WindowsBehavior) {
+    std::string filePath = "C:\\path\\to\\file.txt";
+    std::string expectedFile = ".txt";
+    EXPECT_EQ(getExtension(filePath), expectedFile);
+}
+
+TEST(PathTest, PathCombine_WindowsBehavior) {
+    std::string a = "C:\\path\\to";
+    std::string b = "file.txt";
+    std::string expected = "C:\\path\\to\\file.txt";
+    EXPECT_EQ(pathCombine(a, b), expected);
+}
 #endif
 
 #ifdef __linux__
@@ -43,5 +97,30 @@ TEST(PathTest, GetDirectoryName_LinuxBehavior) {
     std::string filePath = "/path/to/file.txt";
     std::string expectedDir = "/path/to";
     EXPECT_EQ(getDirectoryName(filePath), expectedDir);
+}
+
+TEST(PathTest, GetFileName_LinuxBehavior) {
+    std::string filePath = "/path/to/file.txt";
+    std::string expectedFile = "file.txt";
+    EXPECT_EQ(getFileName(filePath), expectedFile);
+}
+
+TEST(PathTest, GetFileNameWithoutExtension_LinuxBehavior) {
+    std::string filePath = "/path/to/file.txt";
+    std::string expectedFile = "file";
+    EXPECT_EQ(getFileNameWithoutExtension(filePath), expectedFile);
+}
+
+TEST(PathTest, GetExtension_LinuxBehavior) {
+    std::string filePath = "/path/to/file.txt";
+    std::string expectedFile = ".txt";
+    EXPECT_EQ(getExtension(filePath), expectedFile);
+}
+
+TEST(PathTest, PathCombine_LinuxBehavior) {
+    std::string a = "/path/to";
+    std::string b = "file.txt";
+    std::string expected = "/path/to/file.txt";
+    EXPECT_EQ(pathCombine(a, b), expected);
 }
 #endif
