295268f91b91f0b272e2bdcef396b7adc1715138
Brian Luft <brian@electroly.com>
2025-04-05 03:29:00 -0300

    dev.sh: don't map port 5000

--- build/dev.sh
+++ build/dev.sh
@@ -20,4 +20,4 @@ scripts/depsDownload.sh
 cat files/Dockerfile.build-dev | sed "s/\$IMAGE_NAME/$IMAGE_NAME/g; s/\$HOST_UID/$HOST_UID/g; s/\$HOST_GID/$HOST_GID/g; s/\$ARCH/$ARCH/g; s/\$USER/$USER/g; s/\$GIT_NAME/$GIT_NAME/g; s/\$GIT_EMAIL/$GIT_EMAIL/g" | docker buildx build --progress plain -t $IMAGE_NAME files -f-
 
 cd ..
-docker run --rm ${TTY_FLAG:=--tty --interactive} --volume "$PWD:/code" --workdir /code --name $IMAGE_NAME -p 5000:5000 $IMAGE_NAME "$@"
+docker run --rm ${TTY_FLAG:=--tty --interactive} --volume "$PWD:/code" --workdir /code --name $IMAGE_NAME $IMAGE_NAME "$@"
