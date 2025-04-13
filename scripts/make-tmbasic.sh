7854e8fef587a3f5cb8c99dd92acc2df3dad54e2
Brian Luft <brian@electroly.com>
2025-04-12 23:26:28 -0500

    Add diagnostics to make-tmbasic.sh

--- scripts/make-tmbasic.sh
+++ scripts/make-tmbasic.sh
@@ -28,8 +28,21 @@ if [[ "$TARGET_OS" == "linux" && "$LINUX_DISTRO" == "ubuntu" ]]; then
     TOOLCHAIN_SUFFIX="-dev"
 fi
 
+TOOLCHAIN_FILE="../build/files/cmake-toolchain-${TARGET_OS}-${ARCH}${TOOLCHAIN_SUFFIX}.cmake"
+
+# Log all variables
+set -x
+echo "TARGET_OS: $TARGET_OS"
+echo "LINUX_DISTRO: $LINUX_DISTRO"
+echo "LINUX_TRIPLE: $LINUX_TRIPLE"
+echo "ARCH: $ARCH"
+echo "PREFIX: $PREFIX"
+echo "TOOLCHAIN_SUFFIX: $TOOLCHAIN_SUFFIX"
+echo "TOOLCHAIN_FILE: $TOOLCHAIN_FILE"
+set +x
+
 cmake \
-    "-DCMAKE_TOOLCHAIN_FILE=../build/files/cmake-toolchain-${TARGET_OS}-${ARCH}${TOOLCHAIN_SUFFIX}.cmake" \
+    "-DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE" \
     -DCMAKE_VERBOSE_MAKEFILE=OFF \
     -DTARGET_OS=$TARGET_OS \
     -DLINUX_DISTRO=$LINUX_DISTRO \
