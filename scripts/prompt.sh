8baaf8ced4415dece4d2b8f61ef5a624046cdbaa
Brian Luft <brian@electroly.com>
2025-04-18 23:40:51 -0500

    Make prompt.sh the default shell in the dev container

--- scripts/prompt.sh
+++ scripts/prompt.sh
@@ -15,7 +15,7 @@ do
     echo
     echo
     echo
-    echo -e "\e[1;33mm\e[0make \e[1;33mg\e[0mhpages \e[1;33mc\e[0mlean : \e[1;33mr\e[0mun \e[1;33m1\e[0m6color \e[1;33m2\e[0m56color \e[1;33mt\e[0mest test\e[1;33mF\e[0milter : \e[1;33mf\e[0mormat \e[1;33mu\e[0mpdate"
+    echo -e "\e[1;33mm\e[0make \e[1;33mg\e[0mhpages \e[1;33mc\e[0mlean : \e[1;33mr\e[0mun \e[1;33m1\e[0m6color \e[1;33m2\e[0m56color \e[1;33mt\e[0mest test\e[1;33mF\e[0milter : \e[1;33mf\e[0mormat \e[1;33mu\e[0mpdate : \e[1;33ms\e[0mhell \e[1;33mq\e[0muit"
     echo -n "? "
     read -n1 x
     echo
@@ -58,6 +58,9 @@ do
     elif [ "$x" == "c" ]
     then
         scripts/clean.sh
+    elif [ "$x" == "s" ]
+    then
+        bash -i
     elif [ "$x" == "q" ]
     then
         break
